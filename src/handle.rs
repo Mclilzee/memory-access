f87e6e1e19b11edc65f9d788c5f1df449d65b46a
Emad Ali <mc.lilzee@hotmail.com>
2023-05-29 15:57:14 +0200

    Add handle closing code

--- src/handle.rs
+++ src/handle.rs
@@ -1,11 +1,16 @@
-use windows::Win32::Foundation::HANDLE;
+use windows::Win32::Foundation::{CloseHandle, HANDLE};
 use windows::Win32::System::Threading::OpenProcess;
 use windows::Win32::System::Threading::{PROCESS_VM_READ, PROCESS_VM_WRITE};
 
 pub fn get_read_handle(pid: u32) -> HANDLE {
-    unsafe { OpenProcess(PROCESS_VM_READ, true, pid) }.expect("Error opening handle")
+    unsafe { OpenProcess(PROCESS_VM_READ, true, pid) }.expect("Failed to open read access handle.")
 }
 
 pub fn get_write_handle(pid: u32) -> HANDLE {
-    unsafe { OpenProcess(PROCESS_VM_WRITE, true, pid) }.expect("Error opening handle")
+    unsafe { OpenProcess(PROCESS_VM_WRITE, true, pid) }
+        .expect("Failed to open write access handle.")
+}
+
+pub fn close_handle(handle: HANDLE) {
+    unsafe { CloseHandle(handle) }.expect("Failed to close handle.");
 }
