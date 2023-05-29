e563a3d966aeea6dd0ea6b3a61f21624bce418b3
Emad Ali <mc.lilzee@hotmail.com>
2023-05-29 20:48:42 +0200

    Add error handling to writing memory

--- src/write.rs
+++ src/write.rs
@@ -1,73 +1,104 @@
 use std::ffi::c_void;
-use windows::Win32::Foundation::HANDLE;
+use windows::core::Error;
+use windows::Win32::Foundation::{GetLastError, HANDLE};
 use windows::Win32::System::Diagnostics::Debug::WriteProcessMemory;
 
-pub fn write_u32(handle: HANDLE, address_offset: u32, value: u32) {
+pub fn write_u32(handle: HANDLE, address_offset: u32, value: u32) -> Result<(), Error> {
     let buffer = value.to_le_bytes();
 
-    unsafe {
+    let result = unsafe {
         WriteProcessMemory(
             handle,
             address_offset as *const c_void,
             buffer.as_ptr() as *mut c_void,
             buffer.len(),
             None,
-        );
+        )
+    };
+
+    if result.as_bool() {
+        Ok(())
+    } else {
+        Err(unsafe { GetLastError() }.into())
     }
 }
 
-pub fn write_u16(handle: HANDLE, address_offset: u32, value: u16) {
+pub fn write_u16(handle: HANDLE, address_offset: u32, value: u16) -> Result<(), Error> {
     let buffer = value.to_le_bytes();
 
-    unsafe {
+    let result = unsafe {
         WriteProcessMemory(
             handle,
             address_offset as *const c_void,
             buffer.as_ptr() as *mut c_void,
             buffer.len(),
             None,
-        );
+        )
+    };
+
+    if result.as_bool() {
+        Ok(())
+    } else {
+        Err(unsafe { GetLastError() }.into())
     }
 }
 
-pub fn write_u8(handle: HANDLE, address_offset: u32, value: u8) {
+pub fn write_u8(handle: HANDLE, address_offset: u32, value: u8) -> Result<(), Error> {
     let buffer = value.to_le_bytes();
 
-    unsafe {
+    let result = unsafe {
         WriteProcessMemory(
             handle,
             address_offset as *const c_void,
             buffer.as_ptr() as *mut c_void,
             buffer.len(),
             None,
-        );
+        )
+    };
+
+    if result.as_bool() {
+        Ok(())
+    } else {
+        Err(unsafe { GetLastError() }.into())
     }
 }
 
-pub fn write_f32(handle: HANDLE, address_offset: u32, value: f32) {
+pub fn write_f32(handle: HANDLE, address_offset: u32, value: f32) -> Result<(), Error> {
     let buffer = value.to_le_bytes();
 
-    unsafe {
+    let result = unsafe {
         WriteProcessMemory(
             handle,
             address_offset as *const c_void,
             buffer.as_ptr() as *mut c_void,
             buffer.len(),
             None,
-        );
+        )
+    };
+
+    if result.as_bool() {
+        Ok(())
+    } else {
+        Err(unsafe { GetLastError() }.into())
     }
 }
 
-pub fn write_utf16_string(handle: HANDLE, address_offset: u32, value: &str) {
+pub fn write_utf16_string(handle: HANDLE, address_offset: u32, value: &str) -> Result<(), Error> {
     let buffer = value.encode_utf16().collect::<Vec<u16>>();
 
-    unsafe {
+    let result = unsafe {
         WriteProcessMemory(
             handle,
             address_offset as *const c_void,
             buffer.as_ptr() as *mut c_void,
             buffer.len(),
             None,
-        );
+        )
+    };
+
+    if result.as_bool() {
+        Ok(())
+    } else {
+        Err(unsafe { GetLastError() }.into())
     }
 }
