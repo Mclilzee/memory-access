671e2cbe82fa01ab948a327f1717b41cfbd813bc
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-07-03 20:45:26 +0200

    Update interface

--- src/read.rs
+++ src/read.rs
@@ -8,71 +8,7 @@ use windows::Win32::System::Diagnostics::Debug::ReadProcessMemory;
 // requires a proper handle with reading rights, can use the windows_memory_access::handle::get_read_only_handle(pid); to
 // simplify getting handles and windows_memory_access::handle::close_handle(handle); to close.
 
-pub fn read_u32(handle: HANDLE, address_offset: u32) -> Result<u32, Error> {
-    let mut buffer = [0u8; 4];
-
-    unsafe {
-        ReadProcessMemory(
-            handle,
-            address_offset as *const c_void,
-            buffer.as_mut_ptr() as *mut c_void,
-            buffer.len(),
-            None,
-        )?
-    };
-
-    Ok(u32::from_le_bytes(buffer))
-}
-
-pub fn read_u16(handle: HANDLE, address_offset: u32) -> Result<u16, Error> {
-    let mut buffer = [0u8; 2];
-
-    unsafe {
-        ReadProcessMemory(
-            handle,
-            address_offset as *const c_void,
-            buffer.as_mut_ptr() as *mut c_void,
-            buffer.len(),
-            None,
-        )?
-    };
-
-    Ok(u16::from_le_bytes(buffer))
-}
-
-pub fn read_u8(handle: HANDLE, address_offset: u32) -> Result<u8, Error> {
-    let mut buffer = [0u8; 1];
-
-    unsafe {
-        ReadProcessMemory(
-            handle,
-            address_offset as *const c_void,
-            buffer.as_mut_ptr() as *mut c_void,
-            buffer.len(),
-            None,
-        )?
-    };
-
-    Ok(u8::from_le_bytes(buffer))
-}
-
-pub fn read_f32(handle: HANDLE, address_offset: u32) -> Result<f32, Error> {
-    let mut buffer = [0u8; 4];
-
-    unsafe {
-        ReadProcessMemory(
-            handle,
-            address_offset as *const c_void,
-            buffer.as_mut_ptr() as *mut c_void,
-            buffer.len(),
-            None,
-        )?
-    };
-
-    Ok(f32::from_le_bytes(buffer))
-}
-
-pub fn read_utf16_string(handle: HANDLE, address_offset: u32) -> Result<String, Error> {
+pub fn utf16_string(handle: HANDLE, address_offset: u32) -> Result<String, Error> {
     let mut buffer = [0u16; 100];
 
     unsafe {
@@ -92,4 +28,60 @@ pub fn read_utf16_string(handle: HANDLE, address_offset: u32) -> Result<String, 
             String::from_utf16_lossy(&utf16_array)
         })
     }
+}
+
+pub fn read_u32(handle: HANDLE, address_offset: u32) -> Result<u32, Error> {
+    u32_bytes(handle, address_offset).map(u32::from_le_bytes)
+}
+
+pub fn read_i32(handle: HANDLE, address_offset: u32) -> Result<i32, Error> {
+    u32_bytes(handle, address_offset).map(i32::from_le_bytes)
+}
+
+fn u32_bytes(handle: HANDLE, address_offset: u32) -> Result<[u8; 4], Error> {
+    let mut buffer = [0u8; 4];
+
+    unsafe {
+        ReadProcessMemory(
+            handle,
+            address_offset as *const c_void,
+            buffer.as_mut_ptr() as *mut c_void,
+            buffer.len(),
+            None,
+        )?
+    };
+
+    Ok(buffer)
+}
+
+fn u16_bytes(handle: HANDLE, address_offset: u32) -> Result<[u8; 2], Error> {
+    let mut buffer = [0u8; 2];
+
+    unsafe {
+        ReadProcessMemory(
+            handle,
+            address_offset as *const c_void,
+            buffer.as_mut_ptr() as *mut c_void,
+            buffer.len(),
+            None,
+        )?
+    };
+
+    Ok(buffer)
+}
+
+fn u8_byte(handle: HANDLE, address_offset: u32) -> Result<u8, Error> {
+    let mut buffer = [0u8; 1];
+
+    unsafe {
+        ReadProcessMemory(
+            handle,
+            address_offset as *const c_void,
+            buffer.as_mut_ptr() as *mut c_void,
+            buffer.len(),
+            None,
+        )?
+    };
+
+    Ok(buffer[0])
 }
